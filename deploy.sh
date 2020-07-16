912e160fe33da1994d9ccfa364cfa051e9f0c433
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-07-16 18:06:22 +0800

    u

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+rm -rf antden.github.io
+gitbook build
+git clone https://github.com/AntDen/antden.github.io
+
+rm -rf antden.github.io/*
+rsync -av _book/ antden.github.io/
+
+cd antden.github.io 
+
+
+git add *
+git commit -m u
+git push
